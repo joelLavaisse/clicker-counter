78af1bc0aad29de92f4f9469eead914546a04bf3
joelLavaisse <joel.lavaisse@mercadolibre.com>
2020-09-23 17:40:30 -0300

    Add localstorage

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
-// Clicker logic
+// Get DOM elements
 const counterContainer = document.getElementById('counter-container');
 const counter = document.getElementById('counter');
+const resetButton = document.getElementById('reset');
 
+// Set value
+function setValue(value) {
+  localStorage.setItem('counter', value);
+  counter.innerHTML = value;
+}
+
+// Clicker +1 logic
 counterContainer.addEventListener('click', () => {
   const counterValue = parseInt(counter.innerHTML)
-  counter.innerHTML = counterValue + 1;
+  const newCounterValue = counterValue + 1;
+  setValue(newCounterValue);
 })
 
 // Reset button logic
-const resetButton = document.getElementById('reset');
-
 resetButton.addEventListener('click', () => {
   const response = confirm('¿Reiniciar el contador?');
   if (response) {
-    counter.innerHTML = 0;
+    setValue(0);
   };
 })
 
-// Load service worker
+
 window.onload = () => {
   'use strict';
 
+  // Load service worker
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('./sw.js');
+  }
+
+  // Load counter state
+  if (!localStorage.getItem('counter')) {
+    setValue(0);
+  } else {
+    const value = parseInt(localStorage.getItem('counter'));
+    counter.innerHTML = value;
   }
 }
